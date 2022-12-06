ea821964f0aa5fcd087455736e7c0d732c920079
1141288684 <60177676+1141288684@users.noreply.github.com>
2022-12-06 17:09:43 +0800

    Update ddns.py

--- ddns.py
+++ ddns.py
@@ -5,9 +5,9 @@ from requests.adapters import HTTPAdapter
 # dnspod api 动态域名解析————只需要定时执行此脚本
 s = requests.session()
 s.mount('https://', HTTPAdapter(max_retries=5))  # 超时重传次数
-login_token = ''  # API_ID,TOKEN
-domain = ''   # 根域名
-hostname = ''    # 主机名/子域(www)
+login_token = '366561,334c67995bc8b9dd1c1aacb90bbadddc'  # API_ID,TOKEN
+domain = 'koader.top'   # 根域名
+hostname = 'db.test'    # 主机名/子域(www)
 
 
 def get_ip(ipv6: bool = True):
@@ -20,7 +20,7 @@ def get_ip(ipv6: bool = True):
     return d['ip']
 
 
-def get_record_id():
+def get_record_id(ip):
     print("获取dns记录ing，可能会卡")
     login = None
     url = 'https://dnsapi.cn/Record.List'
@@ -33,17 +33,20 @@ def get_record_id():
     except requests.exceptions.RequestException:
         print("请求超时，请重试")
     rs = json.loads(login.text)  # 转为dict
-    record_id = None
     for i in rs['records']:
         if i['name'] == hostname:
-            record_id = i['id']
-    print("dns记录id:" + record_id)
-    return record_id
+            if i['value'] != ip:
+                record_id = i['id']
+                print("dns记录id:" + record_id)
+                return record_id
+            else:
+                print('ip没有发生变化，停止运行')
+                exit()
 
 
 def update_record(ip):
     rs = None
-    record_id = get_record_id()
+    record_id = get_record_id(ip)
     print("更改解析ing，可能会卡")
     url = 'https://dnsapi.cn/Record.Ddns'
     form = {
